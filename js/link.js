0e7ae45d6b5b893aa063e82b41dd18ddd1cbdcab
nelsonwp <44693711+nelsonwp@users.noreply.github.com>
2019-11-25 10:38:07 +0800

    Update link.js

--- js/link.js
+++ js/link.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
-    $(".head").load("http://localhost/blog_web/html/head.html");
-    $(".banners").load("http://localhost/blog_web/html/banner.html");
-    $(".video").load("http://localhost/blog_web/php/sy_video.php");
-    $(".name_list").load("http://localhost/blog_web/php/sy_name_list.php");
-    $(".rili").load("http://localhost/blog_web/html/rili.html");
-    $(".foot").load("http://localhost/blog_web/html/foot.html");
-    $("#mrjz").load("http://localhost/blog_web/php/mryj.php");
-    $(".link").load("http://localhost/blog_web/php/yqlink.php");
-	$(".grxd").load("http://localhost/blog_web/php/newgrxd_list.php");
+    $(".head").load("https://nelsonwp.github.io/blog/html/head.html");
+    $(".banners").load("https://nelsonwp.github.io/blog/html/banner.html");
+    $(".video").load("https://nelsonwp.github.io/blog/php/sy_video.php");
+    $(".name_list").load("https://nelsonwp.github.io/blog/php/sy_name_list.php");
+    $(".rili").load("https://nelsonwp.github.io/blog/html/rili.html");
+    $(".foot").load("https://nelsonwp.github.io/blog/html/foot.html");
+    $("#mrjz").load("https://nelsonwp.github.io/blog/php/mryj.php");
+    $(".link").load("https://nelsonwp.github.io/blog/php/yqlink.php");
+	$(".grxd").load("https://nelsonwp.github.io/blog/php/newgrxd_list.php");
 });
 function aj(obj){
     var Cort=obj.value;
     $.ajax({
         type:"post",
         datatype: "JSON",
-        url:"http://localhost/blog_web/php/sy_name_list.php?lm="+Cort,
+        url:"https://nelsonwp.github.io/blog/php/sy_name_list.php?lm="+Cort,
         async:false,
         success:function(){
-            $(".name_list").load("http://localhost/blog_web/php/sy_name_list.php?lm="+Cort);
+            $(".name_list").load("https://nelsonwp.github.io/blog/php/sy_name_list.php?lm="+Cort);
         }
     })
 
